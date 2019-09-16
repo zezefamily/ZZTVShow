f714df2c62e94c7c31d083a40d546b67cfb17033
guoyuze <yuze.guo@bestmath.cn>
2019-09-16 18:44:44 +0800

    新建[小锅的]项目

---
+++ ZZTVShow/ZZTVShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZTVShow
+//
+//  Created by 泽泽 on 2019/9/16.
+//  Copyright © 2019 泽泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZTVShow/ZZTVShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZTVShow
+//
+//  Created by 泽泽 on 2019/9/16.
+//  Copyright © 2019 泽泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
